c2649e5f4343f155c76484db213cbc9aa68ce40a
james.chao <james.chao@ailabs.tw>
2025-06-02 15:33:52 +0800

    renew structure; add voice

---
+++ env.sh
@@ -0,0 +1,7 @@
+# article generator
+pip install --upgrade google-api-python-client
+pip uninstall google-generativeai google-api-core googleapis-common-protos google -y
+pip install --upgrade google-genai
+
+# voice generator
+pip install TTS
