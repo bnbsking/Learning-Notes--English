f83c10cace5d50cd5b0a85ea958897e3c6b31f11
bnbsking <bnbsking@gmail.com>
2025-10-16 18:24:25 +0800

    add inter2

--- scripts/interview.py
+++ scripts/interview.py
@@ -3,6 +3,6 @@ from learnen.interview.main import InterviewQuestionGenerator
 if __name__ == "__main__":
     extractor = InterviewQuestionGenerator(
         resume_path = "data/resume.txt",
-        requirements_path = "data/recuiting_requirements_01.txt",
+        requirements_path = "data/interview/02/recruiting_requirements_02.txt",
     )
     extractor.run()
